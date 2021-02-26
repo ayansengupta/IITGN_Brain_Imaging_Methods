7cf2714d6752e1bd4bb145467d9d87f68f2b0b40
Ayan Sengupta <uam111@gmail.com>
2021-02-26 05:33:21 +0000

    Update psychopy_demo.py
    changed the position to -8 and reduced the sf

--- Classwork/psychopy_demo.py
+++ Classwork/psychopy_demo.py
@@ -1,10 +1,16 @@
 from psychopy import visual, core  # import some libraries from PsychoPy
 
 #create a window
+#the demo monitor specs are as follows 
+#Aspect Ratio (pixel): 1024 x 768
+#Screen width (cm): 30
+#screen distance (cm): 30
+
 mywin = visual.Window([800,600], monitor="demo_monitor", units="deg")
 
+
 #create some stimuli
-grating = visual.GratingStim(win=mywin, mask="circle", size=10, pos=[-5,0], sf=10)
+grating = visual.GratingStim(win=mywin, mask="circle", size=10, pos=[-8,0], sf=3)
 fixation = visual.GratingStim(win=mywin, size=0.5, pos=[0,0], sf=0, rgb=-1)
 
 #draw the stimuli and update the window
@@ -14,6 +20,10 @@ mywin.update()
 
 #pause, so you get a chance to see it!
 core.wait(10.0)
+
+
+
+# creating the dynamic stimulation
 frame_refresh=50
 trial_duration=10
 number_of_trials=1
